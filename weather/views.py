3742931f80ee7ae297056a344899aa7e06196f82
Denys Smyk <smukdenys10@gmail.com>
2020-01-29 22:48:59 +0200

    connected to OpenWeather

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,19 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = 'bbd0d8b39f32054e1f3b42ed8e26f310'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    city = 'London'
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city':city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
